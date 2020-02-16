0535e4891ab1daae96e8315cb7ea8ce2e71cdde5
Ali Abdallah <ali.alaa5547@gmail.com>
2020-02-16 22:10:48 +0200

    BroadCast file Update

--- BroadCast.py
+++ BroadCast.py
@@ -7,13 +7,24 @@ with open("users.txt", "r", encoding="utf-8") as file:
 	lines = file.readlines()
 	url = "https://api.telegram.org/bot<Token>/"
 	count = 0
+	Error = 0
 	for line in lines:
-		result = re.search(r'[0-9]+', line) # Searching for chat_id
-		message = """ <Message> """ # The message that will send to the users
+		result = re.search(r'[0-9]+', line)  # Searching for chat_id
+		message = """ <Message> """  # The message that will send to the users
 		params = {"chat_id": result[0], "text": message}
 		response = requests.post(url + "sendMessage", data=params)
 		print("\n")
-		print(response)
-		print(f"The message has been send to id={result}")
-		count += 1
-	print(count)
+		print(response.status_code)
+		if response.status_code == 200:
+			print(response.status_code)
+			print(f"The message has been send to id={result}")
+			count += 1
+		else:
+			print(response.status_code)
+			print(f"Error to send message to id={result}")
+			Error += 1
+	print("\n")
+	print(f"Success={count}")
+	print("\n")
+	print(f"Error={Error}")
+	print("\n")
